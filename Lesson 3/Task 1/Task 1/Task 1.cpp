7dfdb435ee584f890289bbd8f294afaa9c308ef9
Татьяна Кочубейник <kisa7392@mail.ru>
2022-07-17 23:55:30 +0300

    Third version

--- Lesson 3/Task 1/Task 1/Task 1.cpp
+++ Lesson 3/Task 1/Task 1/Task 1.cpp
@@ -7,45 +7,38 @@ class Calculator
 {
 private:
     double num1, num2;
-    double a;
 public:
     double add(double num1, double num2) {
-       a = num1 + num2;
-       return a;
+       return num1 + num2;
     }
     double multiply(double num1, double num2) {
-        a = num1 * num2;
-        return a;
+        return num1 * num2;
     }
     double subtract_1_2(double num1, double num2) {
-        a = num1 - num2;
-        return a;
+        return num1 - num2;
     }
     double subtract_2_1(double num1, double num2) {
-        a = num2 - num1;
-        return a;
+        return num2 - num1;
     }
     double divide_1_2(double num1, double num2) {
-        a = num1 / num2;
-        return a;
+        return num1 / num2;
     }
     double divide_2_1(double num1, double num2) {
-        a = num2 / num1;
-        return a;
+        return num2 / num1;
     }
     bool set_num1(double num1) {
-        this->num1 = num1;
-        if (num1 == 0) {
-            return false;
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
         }
-        else return true;
+        else { return false; }
     }
     bool set_num2(double num2) {
-        this->num2 = num2;
-        if (num2 == 0) { 
-            return false; 
+        if (num2 != 0) { 
+            this->num2 = num2;
+            return true; 
         }
-        else return true;
+        else { return false; }
     }
 };
 
