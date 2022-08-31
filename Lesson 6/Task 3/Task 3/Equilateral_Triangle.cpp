4ef9e89b24ebb4022db03a3b893d81f031ece514
Татьяна Кочубейник <kisa7392@mail.ru>
2022-08-31 23:57:47 +0300

    Fourth version

--- Lesson 6/Task 3/Task 3/Equilateral_Triangle.cpp
+++ Lesson 6/Task 3/Task 3/Equilateral_Triangle.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include <string>
 #include "Equilateral_Triangle.h"
 
 Equilateral_Triangle::Equilateral_Triangle() : Triangle(30, 30, 30, 60, 60, 60) {};
 std::string Equilateral_Triangle::name() {
+    setlocale(LC_ALL, "Russian");
     return "Равносторонний треугольник: ";
 }
